dc5c095a49247678c948bc25efabf95566a8d901
samysep17 <samysep17@gmail.com>
2016-10-10 18:07:40 -0500

    Add files via upload

---
+++ Proyecto 2016/5octubre.R
@@ -0,0 +1,43 @@
+FN2004F<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2004.Femenino.csv", stringsAsFactors = FALSE)
+FN2004M<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2004.Masculino.csv", stringsAsFactors = FALSE)
+FN2004G<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2004.General.csv", stringsAsFactors = FALSE)
+
+FN2005F<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2005.Femenino.csv", stringsAsFactors = FALSE)
+FN2005M<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2005.Masculino.csv", stringsAsFactors = FALSE)
+FN2005G<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2005.General.csv", stringsAsFactors = FALSE)
+
+FN2006F<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2006.Femenino.csv", stringsAsFactors = FALSE)
+FN2006M<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2006.Masculino.csv", stringsAsFactors = FALSE)
+FN2006G<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2006.General.csv", stringsAsFactors = FALSE)
+
+FN2007F<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2007.Femenino.csv", stringsAsFactors = FALSE)
+FN2007M<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2007.Masculino.csv", stringsAsFactors = FALSE)
+FN2007G<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2007.General.csv", stringsAsFactors = FALSE)
+
+FN2008F<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2008.Femenino.csv", stringsAsFactors = FALSE)
+FN2008M<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2008.Masculino.csv", stringsAsFactors = FALSE)
+FN2008G<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2008.General.csv", stringsAsFactors = FALSE)
+
+FN2009F<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2009.Femenino.csv", stringsAsFactors = FALSE)
+FN2009M<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2009.Masculino.csv", stringsAsFactors = FALSE)
+FN2009G<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2009.General.csv", stringsAsFactors = FALSE)
+
+FN2010F<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2010.Femenino.csv", stringsAsFactors = FALSE)
+FN2010M<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2010.Masculino.csv", stringsAsFactors = FALSE)
+FN2010G<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2010.General.csv", stringsAsFactors = FALSE)
+
+FN2011F<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2011.Femenino.csv", stringsAsFactors = FALSE)
+FN2011M<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2011.Masculino.csv", stringsAsFactors = FALSE)
+FN2011G<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2011.General.csv", stringsAsFactors = FALSE)
+
+FN2012F<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2012.Femenino.csv", stringsAsFactors = FALSE)
+FN2012M<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2012.Masculino.csv", stringsAsFactors = FALSE)
+FN2012G<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2012.General.csv", stringsAsFactors = FALSE)
+
+FN2013F<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2013.Femenino.csv", stringsAsFactors = FALSE)
+FN2013M<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2013.Masculino.csv", stringsAsFactors = FALSE)
+FN2013G<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2013.General.csv", stringsAsFactors = FALSE)
+
+FN2014F<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2014.Femenino.csv", stringsAsFactors = FALSE)
+FN2014M<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2014.Masculino.csv", stringsAsFactors = FALSE)
+FN2014G<-read.csv("D:\\Proyecto 2016\\Archivos CSV\\tabula-2014.General.csv", stringsAsFactors = FALSE)
